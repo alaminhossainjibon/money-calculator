59fde25562a979b22ca05d0f841904550447f3ed
alaminhossainjibon <mdalamin7622@gmail.com>
2022-02-17 16:27:06 +0600

    error alert added

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//Earning Amount and Pocket Money function
+function getValue(input) {
+    const inputField = document.getElementById(input);
+    const inputValue = parseInt(inputField.value);
+    inputField.value = '';
+    return inputValue;
+}
+// Earning Amount and Pocket Money calculate.
+document.getElementById('calculate').addEventListener('click', function () {
+    const income = getValue('income');
+    const meal = getValue('meal');
+    const pay = getValue('pay');
+    const clothing = getValue('clothing');
+
+    // Error alert.
+    if (income < 0 || meal < 0 || pay < 0 || clothing < 0) {
+        alert('Please enter your positive Amount!!!');
+    }
+    else {
+        //Pocket Money add
+        const cost = meal + pay + clothing;
+        document.getElementById('pocket-money').innerText = cost;
+        document.getElementById('total-balance').innerText = income - cost;
+    }
+});
+
+// blank  error
+calculate.addEventListener('click', function () {
+    if (income.value == '' || meal.value == '' || pay.value == '' || clothing.value == '') {
+        alert('Please enter your Value!!!');
+    }
+    else {
+        const cost = meal + pay + clothing;
+        document.getElementById('pocket-money').innerText = cost;
+        document.getElementById('total-balance').innerText = income - cost;
+    }
+});
+
